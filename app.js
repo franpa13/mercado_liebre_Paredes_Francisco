242c081e0d58c777fd2cafe189e809c4202f46e6
David Lescano <lescanodavid07@gmail.com>
2023-07-12 10:53:52 -0300

    1ra clase - HTML

---
+++ app.js
@@ -0,0 +1,27 @@
+// Requerir las dependencias
+const  express = require('express');
+const app = express();
+const path = require('path');
+
+// Poner a disposicion el contenido de la carpeta  'public' desde cualquier archivo / ruta
+// app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+app.listen( PORT, () => console.log(`Server up:  PORT:${PORT}`) );
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/product.html'))
+})
+
+
+
+
+
+
+
